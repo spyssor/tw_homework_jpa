300e2b2cb53c84250d578ece0bdc827896dd44c8
huyf <spyssor@gmail.com>
2019-05-27 16:16:31 +0800

    编写找出第一个姓名包含‘n’字符的雇员的jpa并通过测试用例

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -19,6 +19,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
 
+
     //4.实现对Employee的分页查询，每页两个数据
 
     //5.查找**的所在的公司的公司名称
--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -50,13 +50,13 @@ public class EmployeeJPATest {
         assertThat(actualName).isEqualTo(expectedEmployee.getName());
     }
 
-//    @Test
-//    public void should_return_employee_given_character_in_name_and_salary_large_than() throws Exception {
-//        //2.找出Employee表中第一个姓名包含`n`字符的雇员所有个人信息
-//        Employee expectedEmployee = new Employee(1,"xiaohong",19,"female",1,7000);
-//        String actualName = null;
-//        assertThat(actualName).isEqualTo(expectedEmployee.getName());
-//    }
+    @Test
+    public void should_return_employee_given_character_in_name_and_salary_large_than() throws Exception {
+        //2.找出Employee表中第一个姓名包含`n`字符的雇员所有个人信息
+        Employee expectedEmployee = new Employee(1,"xiaohong",19,"female",1,7000);
+        String actualName = employeeRepository.findOneByWhoNameIncludeAndSalaryGreatThan("n", 6000).getName();
+        assertThat(actualName).isEqualTo(expectedEmployee.getName());
+    }
 
 //    @Test
 //    public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() throws Exception {
