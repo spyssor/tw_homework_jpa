a5acdb06fee46cda46416c5df6f9e577bb231fae
huyf <spyssor@gmail.com>
2019-05-27 15:34:01 +0800

    补充entity的Company和Employee内容

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,20 @@
 package com.example.employee.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Company {
 
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String companyName;
+
+    private Integer employeesNumber;
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,26 @@
 package com.example.employee.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Employee {
 
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String gender;
+
+    private Integer companyId;
+
+    private Integer salary;
 }
