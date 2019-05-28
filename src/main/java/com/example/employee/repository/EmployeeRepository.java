4ced65186c11dca8f8c19e96927fe94dd9d3b845
huyf <spyssor@gmail.com>
2019-05-28 22:59:03 +0800

    修改由配置yml引起的错误：驼峰无法识别，而被识别成_形式

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -8,16 +8,16 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
-    @Query(nativeQuery = true, value = "select * from employee where name = ?1 limit 1")
+    @Query(nativeQuery = true, value = "select * from employee  where name = ?1 limit 1")
     Employee findOneByEmployeeName(String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
-    @Query(nativeQuery = true, value = "select * from employee where name like %?1% and salary > ?2")
+    @Query(nativeQuery = true, value = "select * from employee  where name like %?1% and salary > ?2")
     Employee findOneByWhoNameIncludeAndSalaryGreatThan(String name, Integer salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
@@ -27,7 +27,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     String findOneByWhoSalaryIsTopAndCompanyId(Integer companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
-    @Query(nativeQuery = true, value = "select * from employee")
+    @Query(nativeQuery = true, value = "select * from employee ")
     Page<Employee> list(Pageable pageable);
 
     //5.查找**的所在的公司的公司名称
